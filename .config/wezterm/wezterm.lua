450a8e92011664b9e816364821d3c6f8db3781ef
udunus <104630117+udunuss@users.noreply.github.com>
2024-09-08 13:24:52 +0300

    Add files via upload

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,5 +5,5 @@ config.font = wezterm.font("MesloLGM Nerd Font")
 config.freetype_load_flags = "NO_HINTING"
 config.color_scheme = "Belge (terminal.sexy)"
 config.font_size = 12
-window_background_opacity = 0.5
+config.window_background_opacity = 0.7
 return config
