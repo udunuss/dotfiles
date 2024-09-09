55061bf8e9272f4d288f02ddbd7e893869b78ee9
udunus <104630117+udunuss@users.noreply.github.com>
2024-09-09 15:01:09 +0300

    Add files via upload

--- .config/hypr/scripts/fzflauncher.sh
+++ .config/hypr/scripts/fzflauncher.sh
@@ -1,40 +1,64 @@
-#!/bin/bash
 
+#!/bin/bash
 # ANSI color codes
 RESET="\e[0m"
 DIM="\e[90m"
 RED="\e[31m"
-# Function to get list of applications
+
+# File to store app launch frequencies
+LAUNCH_LOG="$HOME/.app-launch-frequency"
+
+# Function to get launch frequency
+get_frequency() {
+    local app_name="$1"
+    grep -P "^${app_name}\|" "$LAUNCH_LOG" 2>/dev/null | cut -d'|' -f2 || echo 0
+}
+
+# Function to increment launch frequency
+increment_frequency() {
+    local app_name="$1"
+    local current_freq=$(get_frequency "$app_name")
+    if grep -q "^${app_name}|" "$LAUNCH_LOG"; then
+        sed -i "s/^${app_name}|.*/${app_name}|$((current_freq + 1))/" "$LAUNCH_LOG"
+    else
+        echo "${app_name}|1" >> "$LAUNCH_LOG"
+    fi
+}
+
+# Function to get list of applications with frequency
 get_applications() {
     while IFS= read -r -d '' file; do
         name=$(awk -F= '/^Name=/{print $2; exit}' "$file")
-        
+
         if [ -n "$name" ]; then
-            echo -e "$name${DIM}|$file|${RESET}"
+            freq=$(get_frequency "$name")
+            echo -e "$freq|$name${DIM}|$file|${RESET}"
         fi
     done < <(find /usr/share/applications ~/.local/share/applications -name "*.desktop" -print0)
 }
 
 # Function to launch the selected application
 launch_application() {
-    desktop_file=$(echo "$1" | cut -d'|' -f2 )
+    local app_name=$(echo "$1" | cut -d'|' -f2)
+    local desktop_file=$(echo "$1" | cut -d'|' -f3)
     if [ -f "$desktop_file" ]; then
         nohup dex "$desktop_file"
         if [ $? -ne 0 ]; then
-            notify-send "Failed to launch $1"
+            notify-send "Failed to launch $app_name"
+        else
+            increment_frequency "$app_name"
         fi
     else
         notify-send "Desktop file not found: $desktop_file"
     fi
 }
 
-
 # Main script
-selected_app=$(get_applications | sort | fzf --ansi \
+selected_app=$(get_applications | sort -t'|' -k1,1nr -k2,2 | fzf --ansi \
     --cycle --multi --bind 'tab:toggle-down,change:first' \
     --prompt="Select an application: " \
     --layout=reverse \
-    --preview "echo -e \"${RED}\$(echo {} | cut -c1-1 | figlet -f 3d)${RESET}\" " \
+    --preview "echo -e \"${RED}\$(echo {} | cut -d'|' -f2 | cut -c1-1 | figlet -f 3d)${RESET}\" " \
     --preview-window=right:10,border-left)
 
 if [ -n "$selected_app" ]; then
@@ -42,3 +66,4 @@ if [ -n "$selected_app" ]; then
 else
     echo "No application selected."
 fi
+
