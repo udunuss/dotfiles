64c36ca3e2017d5b9a2cb9aff5e548634b6ecb77
udunus <104630117+udunuss@users.noreply.github.com>
2024-09-09 13:45:32 +0300

    Add files via upload

--- .config/hypr/popups/launcher.sh
+++ .config/hypr/popups/launcher.sh
@@ -1,5 +1,5 @@
-width=45
-height=13
+width=85
+height=8
 font_size=15
 pth=~/.config/hypr/scripts/fzflauncher.sh
 ~/.config/hypr/hypr_popup.sh $pth $width $height $font_size
--- .config/hypr/scripts/fzflauncher.sh
+++ .config/hypr/scripts/fzflauncher.sh
@@ -3,21 +3,21 @@
 # ANSI color codes
 RESET="\e[0m"
 DIM="\e[90m"
-
+RED="\e[31m"
 # Function to get list of applications
 get_applications() {
     while IFS= read -r -d '' file; do
         name=$(awk -F= '/^Name=/{print $2; exit}' "$file")
+        
         if [ -n "$name" ]; then
-            echo -e "$name${DIM}|$file${RESET}"
+            echo -e "$name${DIM}|$file|${RESET}"
         fi
     done < <(find /usr/share/applications ~/.local/share/applications -name "*.desktop" -print0)
 }
 
 # Function to launch the selected application
 launch_application() {
-    desktop_file=$(echo "$1" | awk -F'|' '{print $2}' | sed 's/\x1b\[0m$//')
-    
+    desktop_file=$(echo "$1" | cut -d'|' -f2 )
     if [ -f "$desktop_file" ]; then
         nohup dex "$desktop_file"
         if [ $? -ne 0 ]; then
@@ -28,8 +28,13 @@ launch_application() {
     fi
 }
 
+
 # Main script
-selected_app=$(get_applications | sort | fzf --ansi --prompt="Select an application: " --layout=reverse --border)
+selected_app=$(get_applications | sort | fzf --ansi \
+    --prompt="Select an application: " \
+    --layout=reverse \
+    --preview "echo -e \"${RED}\$(echo {} | cut -c1-1 | figlet -f 3d)${RESET}\" " \
+    --preview-window=right:10,border-left)
 
 if [ -n "$selected_app" ]; then
     launch_application "$selected_app"
