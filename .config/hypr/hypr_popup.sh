aa5dcc2e6fc6e4296852e5db50c226da2c37531c
udunus <104630117+udunuss@users.noreply.github.com>
2024-09-11 18:52:41 +0300

    Add files via upload

--- .config/hypr/hypr_popup.sh
+++ .config/hypr/hypr_popup.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-    echo 'please add a script path'
-    exit 0
+if [[ $# -lt 4 ]]; then
+    echo "Usage: $0 <script_path> <cols> <rows> <font_size>"
+    exit 1
 fi
-# Function to get the focused window class
-get_focused_window_class() {
-    hyprctl activewindow -j
+# Change focus mode to click to focus
+hyprctl keyword input:follow_mouse 2
+hyprctl keyword input:float_switch_override_focus 0
+# Function to get the focused window (returns window ID)
+get_focused_window() {
+    hyprctl activewindow -j | jq -r '.address'
 }
 
 # Function to get current focus mode
 get_focus_mode() {
-    #hyprctl getoption input:follow_mouse -j | jq -r .int
 
     grep "follow_mouse" ~/.config/hypr/hyprland.conf | cut -d '=' -f 2 | cut -d '#' -f 1 | tr -d ' '
 }
 get_s_o_f() {
-    #hyprctl getoption input:float_switch_override_focus -j | jq -r .int
 
     grep "float_switch_override_focus" ~/.config/hypr/hyprland.conf | cut -d '=' -f 2 | cut -d '#' -f 1 | tr -d ' '
 }
+# Function to get the window ID by process PID
+get_window_by_pid() {
+    local pid=$1
+    hyprctl clients -j | jq -r --arg pid "$pid" '.[] | select(.pid == ($pid | tonumber)) | .address'
+}
+
 # Save the current focus mode
 ORIGINAL_FOCUS_MODE=$(get_focus_mode)
 ORIGINAL_S_O_F=$(get_s_o_f)
 # Global variable for the WezTerm PID
-WEZTERM_PID=""
+TERMINAL_PID=""
 
 # Cleanup function
 cleanup() {
     # Kill the power menu if it's still running
-    if [ -n "$WEZTERM_PID" ]; then
-        kill $WEZTERM_PID 2>/dev/null
+    if [ -n "$TERMINAL_PID" ]; then
+        kill $TERMINAL_PID 2>/dev/null
     fi
 
     # Restore the original focus mode
@@ -41,26 +48,24 @@ cleanup() {
 # Set trap for cleanup
 trap cleanup EXIT INT TERM
 
-# Change focus mode to click to focus
-hyprctl keyword input:follow_mouse 2
-hyprctl keyword input:float_switch_override_focus 0
+
 # Launch the power menu
 wezterm --config initial_cols=$2 --config initial_rows=$3 --config font_size=$4 start --class floating "$1" &
 
 # Get the PID of the launched wezterm process
-WEZTERM_PID=$!
+TERMINAL_PID=$!
 
 # Wait a bit for the window to appear
 sleep 0.5
 
-# Get the initial focused window class
-INITIAL_FOCUS=$(get_focused_window_class)
+# Get the initial focused window
+INITIAL_FOCUS=$(get_window_by_pid "$TERMINAL_PID")
 
 # Monitor focus
 while true; do
     
 
-    CURRENT_FOCUS=$(get_focused_window_class)
+    CURRENT_FOCUS=$(get_focused_window)
     
     # If focus has changed from the power menu window, exit
     if  [ "$CURRENT_FOCUS" != "$INITIAL_FOCUS" ]; then
@@ -68,7 +73,7 @@ while true; do
     fi
     
     # Check if the wezterm process is still running
-    if ! kill -0 $WEZTERM_PID 2>/dev/null; then
+    if ! kill -0 $TERMINAL_PID 2>/dev/null; then
         break
     fi
     
