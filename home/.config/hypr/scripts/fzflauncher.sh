e65b1fd4711b6c5630e1f2de4ede1da21e17c59c
udunus <104630117+udunuss@users.noreply.github.com>
2024-09-13 00:07:13 +0300

    Update fzflauncher.sh

--- home/.config/hypr/scripts/fzflauncher.sh
+++ home/.config/hypr/scripts/fzflauncher.sh
@@ -1,7 +1,11 @@
-
 #!/bin/bash
+
+set -euo pipefail
+
+# Set terminal title
 title="Launcher"
-echo -e '\033k'$title'\033\\'
+echo -ne "\033]0;${title}\007"
+
 # ANSI color codes
 RESET="\e[0m"
 DIM="\e[90m"
@@ -13,54 +17,58 @@ LAUNCH_LOG="$HOME/.app-launch-frequency"
 # Function to get launch frequency
 get_frequency() {
     local app_name="$1"
-    grep -P "^${app_name}\|" "$LAUNCH_LOG" 2>/dev/null | cut -d'|' -f2 || echo 0
+    grep "^${app_name}|" "$LAUNCH_LOG" 2>/dev/null | cut -d'|' -f2 || echo 0
 }
 
 # Function to increment launch frequency
 increment_frequency() {
     local app_name="$1"
-    local current_freq=$(get_frequency "$app_name")
-    if grep -q "^${app_name}|" "$LAUNCH_LOG"; then
+    local current_freq
+    current_freq=$(get_frequency "$app_name")
+    if grep -q "^${app_name}|" "$LAUNCH_LOG" 2>/dev/null; then
         sed -i "s/^${app_name}|.*/${app_name}|$((current_freq + 1))/" "$LAUNCH_LOG"
     else
         echo "${app_name}|1" >> "$LAUNCH_LOG"
     fi
 }
 
-# Function to get list of applications without ANSI codes for sorting
+# Function to get list of applications
 get_applications() {
-    app_dirs=()
-    for dir in $(echo "$XDG_DATA_DIRS" | tr ':' '\n' | tr -s /); do
-        if [ -d  "$dir/applications" ]; then
-            clean_dir=$(echo "$dir/applications" | tr -s /)
-            app_dirs+=($clean_dir)
+    local app_dirs=()
+    IFS=':' read -ra xdg_dirs <<< "$XDG_DATA_DIRS"
+    for dir in "${xdg_dirs[@]}"; do
+        dir="${dir%/}/applications"
+        if [ -d "$dir" ]; then
+            app_dirs+=("$dir")
         fi
     done
-     local_app_directory=("$HOME/.local/share/applications")
-    if [[ ! " ${app_dirs[@]} " =~ " ${local_app_directory} " ]]; then
-        app_dirs+=("$local_app_directory")
-    fi
-    while IFS= read -r -d '' file; do
-        name=$(awk -F= '/^Name=/{print $2; exit}' "$file")
 
+    local local_app_dir="$HOME/.local/share/applications"
+    if [[ ! " ${app_dirs[*]} " =~ " ${local_app_dir} " ]]; then
+        app_dirs+=("$local_app_dir")
+    fi
+
+    find "${app_dirs[@]}" -name "*.desktop" -print0 | while IFS= read -r -d '' file; do
+        local name
+        name=$(awk -F= '/^Name=/{print $2; exit}' "$file")
         if [ -n "$name" ]; then
+            local freq
             freq=$(get_frequency "$name")
             # Output frequency and app info without ANSI codes for sorting
-            echo -e "$freq|$name|$file"
+            printf "%s|%s|%s\n" "$freq" "$name" "$file"
         fi
-    done < <(find "${app_dirs[@]}" -name "*.desktop" -print0)
+    done
 }
 
 # Function to launch the selected application
 launch_application() {
-    local app_name=$(echo "$1" | cut -d'|' -f2)
-    local desktop_file=$(echo "$1" | cut -d'|' -f3)
+    local IFS='|'
+    read -r freq app_name desktop_file <<< "$1"
     if [ -f "$desktop_file" ]; then
-        nohup dex "$desktop_file" &
-        if [ $? -ne 0 ]; then
-            notify-send "Failed to launch $app_name"
-        else
+        if (nohup dex "$desktop_file" &); then
             increment_frequency "$app_name"
+        else
+            notify-send "Failed to launch $app_name"
         fi
     else
         notify-send "Desktop file not found: $desktop_file"
@@ -70,18 +78,17 @@ launch_application() {
 # Main script
 selected_app=$(get_applications | sort -t'|' -k1,1nr -k2,2 | while IFS='|' read -r freq name file; do
     # Add ANSI escape codes (dimming the frequency)
-    echo -e "${DIM}$freq|${RESET}$name${DIM}|$file${RESET}"
+    printf "${DIM}%s|${RESET}%s${DIM}|%s${RESET}\n" "$freq" "$name" "$file"
 done | fzf --ansi \
      --nth=1 \
-    --cycle --multi --bind 'tab:toggle-down,change:first' \
-    --prompt="Select an application: " \
-    --layout=reverse \
-    --preview "echo -e \"${RED}\$(echo {} | cut -d'|' -f2 | cut -c1-1 | figlet -f 3d)${RESET}\" " \
-    --preview-window=right:11,border-left)
+     --cycle --bind 'tab:toggle+down' \
+     --prompt="Select an application: " \
+     --layout=reverse \
+     --preview "echo -e \"${RED}\$(echo {} | cut -d'|' -f2 | cut -c1-1 | figlet -f 3d)${RESET}\" " \
+     --preview-window=right:11,border-left)
 
-if [ -n "$selected_app" ]; then
+if [ -n "${selected_app:-}" ]; then
     launch_application "$selected_app"
 else
     echo "No application selected."
 fi
-
