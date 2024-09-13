25e5f312c16c060a3b5438e2f3ec2d9a24cafe66
udunus <104630117+udunuss@users.noreply.github.com>
2024-09-13 10:23:24 +0300

    Add files via upload

---
+++ home/.config/hyprpopup/popups/launcher.sh
@@ -0,0 +1,6 @@
+title="Launcher"
+width=86
+height=8
+font_size=15
+pth=~/.config/hyprpopup/scripts/fzflauncher.sh
+~/.config/hyprpopup/hypr_popup.sh $pth $width $height $font_size $title
---
+++ home/.config/hyprpopup/popups/powermenu.sh
@@ -0,0 +1,5 @@
+width=14
+height=5
+font_size=20
+pth=~/.config/hyprpopup/scripts/fzfpowermenu.sh
+~/.config/hyprpopup/hypr_popup.sh $pth $width $height $font_size
