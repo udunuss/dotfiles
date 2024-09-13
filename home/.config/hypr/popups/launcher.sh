39ebb82617f56b1777ea41a10985febaf09fceef
udunus <104630117+udunuss@users.noreply.github.com>
2024-09-13 10:23:39 +0300

    Delete home/.config/hypr/popups directory

--- home/.config/hypr/popups/launcher.sh
+++
@@ -1,6 +0,0 @@
-title="Launcher"
-width=86
-height=8
-font_size=15
-pth=~/.config/hypr/scripts/fzflauncher.sh
-~/.config/hypr/hypr_popup.sh $pth $width $height $font_size $title
--- home/.config/hypr/popups/powermenu.sh
+++
@@ -1,5 +0,0 @@
-width=14
-height=5
-font_size=20
-pth=~/.config/hypr/scripts/fzfpowermenu.sh
-~/.config/hypr/hypr_popup.sh $pth $width $height $font_size
