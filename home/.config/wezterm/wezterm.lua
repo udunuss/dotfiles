759ed698dd2583b38051d3b262ef5b1b17f38a01
udunus <104630117+udunuss@users.noreply.github.com>
2024-12-07 23:44:11 +0300

    Add files via upload

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -6,4 +6,9 @@ config.freetype_load_flags = "NO_HINTING"
 config.color_scheme = "Belge (terminal.sexy)"
 config.font_size = 12
 config.window_background_opacity = 0.7
+if os.getenv("XDG_CURRENT_DESKTOP") == "Hyprland" then
+	config.enable_wayland = false
+else
+	config.enable_wayland = true
+end
 return config
